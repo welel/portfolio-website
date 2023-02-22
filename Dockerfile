bd39e08bea3815c772ab8d8433ab1db59b3b43fa
Pavel Loginov <pavel.loginov.dev@gmail.com>
2023-02-22 20:57:32 +0300

    Update icons, resume; add Dockerfile.

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM node:lts-alpine
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN apk update \
+    && apk add curl bash \
+    && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash \
+    && export NVM_DIR="$HOME/.nvm" \
+    && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" \
+    && nvm install 14.18.1 \
+    && nvm alias default 14.18.1
+
+ENV NVM_DIR="/root/.nvm"
+
+RUN npm install -g npm@9.5.0 --legacy-peer-deps --timeout=10000
+
+RUN npm install -g gatsby-cli
+
+RUN yarn
+
+COPY . .
+
+RUN npm run build
+
+CMD [ "npm", "start" ]
--- src/components/loader.js
+++ src/components/loader.js
@@ -47,20 +47,20 @@ const Loader = ({ finishLoading }) => {
     loader
       .add({
         targets: '#logo path',
-        delay: 300,
-        duration: 1500,
+        delay: 200,
+        duration: 100,
         easing: 'easeInOutQuart',
         strokeDashoffset: [anime.setDashoffset, 0],
       })
       .add({
         targets: '#logo #B',
-        duration: 700,
+        duration: 300,
         easing: 'easeInOutQuart',
         opacity: 1,
       })
       .add({
         targets: '#logo',
-        delay: 500,
+        delay: 200,
         duration: 300,
         easing: 'easeInOutQuart',
         opacity: 0,
@@ -68,7 +68,7 @@ const Loader = ({ finishLoading }) => {
       })
       .add({
         targets: '.loader',
-        duration: 200,
+        duration: 100,
         easing: 'easeInOutQuart',
         opacity: 0,
         zIndex: -1,
